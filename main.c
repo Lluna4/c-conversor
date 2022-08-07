04ef270e6a91fdb31db1104dc390fe216dea9853
Luna <69774917+carlyjb17@users.noreply.github.com>
2022-08-07 03:38:20 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
 int main(void) {
-  char eurodol[15];
+  char eurodol[15]; 
   float nuevo;
   printf("pon si quieres a euros o dolares \n");
-  scanf("%s", eurodol);
-  if (strcmp(eurodol, "euro") != 1) {
+  scanf_s("%s", eurodol);
+  if (strcmp(eurodol, "euro") == 0) {
     printf("pon los dolares \n");
-    scanf("%f", &nuevo);
+    scanf_s("%f", &nuevo);
     float nuevon = nuevo * 0.91;
-    printf("%f", nuevon);
+    printf("%.2f", nuevon);
+    scanf_s("%f", nuevon);
   }
-  else if (strcmp(eurodol, "dolares")) {
+  if (strcmp(eurodol, "dolares") == 0){
     printf("pon los euros \n");
-    scanf("%f", &nuevo);
+    scanf_s("%f", &nuevo);
     float nuevon = nuevo * 1.09;
-    printf("%f", nuevon);
-    
+    printf("%.2f", nuevon); 
+    scanf_s("%f", nuevon);
   }
+  
+  
+}
   
   
 
